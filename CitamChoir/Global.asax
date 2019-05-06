122582f16efffe3caf91eb61dd94b2b94ac4ecf3
munyite-megzs <margaret.sitati1@gmail.com>
2019-05-07 00:26:00 +0300

    Add project files.

---
+++ CitamChoir/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CitamChoir.MvcApplication" Language="C#" %>
---
+++ CitamChoir/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CitamChoir.Startup))]
+namespace CitamChoir
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
