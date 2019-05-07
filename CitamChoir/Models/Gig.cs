79796ead55e1cbe9e7391bbeefebce397628afc0
munyite-megzs <margaret.sitati1@gmail.com>
2019-05-07 13:07:27 +0300

    create gig model

---
+++ CitamChoir/Models/Gig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CitamChoir.Models
+{
+    public class Gig
+    {
+        public ApplicationUser Member { get; set; }
+    }
+}
