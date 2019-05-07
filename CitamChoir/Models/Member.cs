35502283a36075c14aef2af8c94356a98448b4bf
munyite-megzs <margaret.sitati1@gmail.com>
2019-05-07 14:09:13 +0300

    Initial Migration

---
+++ CitamChoir/Models/Member.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CitamChoir.Models
+{
+    public class Member
+    {
+        public int Id { get; set; }
+        [Required]
+        public ApplicationUser VoiceLeader { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string OtherNames { get; set; }
+
+        [Required]
+        [StringLength(10)]
+        public string Gender { get; set; }
+
+        [Required]
+        [StringLength(250)]
+        public Occupation Occupation { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string Phone { get; set; }
+        public DateTime InductionDate { get; set; }
+
+        public bool BGV { get; set; } = false; 
+        public bool WorshipLeader { get; set; } = false;
+        public bool Leader { get; set; } = false;
+
+    }
+}
---
+++ CitamChoir/Models/Occupation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CitamChoir.Models
+{
+    public class Occupation
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(128)]
+        public string Name { get; set; }
+    }
+}
